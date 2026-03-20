969925f9064e7e176e321a58d6a853d930a7a39d
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-20 08:46:56 +0100

    removed padding from main

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -4,7 +4,7 @@
 
 <body class="min-h-screen flex flex-col">
     <x-header />
-    <main class="flex-1 px-4 py-2 flex flex-col">
+    <main class="flex-1 flex">
         {{ $slot }}
     </main>
 </body>
