6e074e0a27144d6ca90d60e91d60336b6627cf75
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-20 08:48:42 +0100

    added routes for placing offers

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\OfferController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name("home");
 
+Route::middleware("auth")->group(function () {
+    Route::get('/nieuw_aanbod', [OfferController::class, "start"])->name("offers.start")->middleware('auth');
+    Route::get('/aanbod_plaatsen', [OfferController::class, "create"])->name("offers.create")->middleware('auth');
+    Route::post('/aanbod_plaatsen', [OfferController::class, "store"])->name("offers.store")->middleware('auth');
 });
