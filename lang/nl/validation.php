c7f0d6bdb62446fbe39e18008b7691cb4bcf737a
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-20 10:10:20 +0100

    Create validation.php

---
+++ lang/nl/validation.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'required' => ':attribute is verplicht.',
+    'string' => ':attribute moet tekst zijn.',
+    'integer' => ':attribute moet een geheel getal zijn.',
+    'decimal' => ':attribute moet een geldig bedrag zijn met :decimal decimalen.',
+    'max' => [
+        'string' => ':attribute mag maximaal :max tekens bevatten.',
+        'numeric' => ':attribute mag niet groter zijn dan :max.',
+    ],
+    'min' => [
+        'string' => ':attribute moet minimaal :min tekens bevatten.',
+        'numeric' => ':attribute moet minimaal :min zijn.',
+    ],
+    'unique' => ':attribute bestaat al.',
+
+    'custom' => [
+        'plate' => [
+            'unique' => 'Voor dit kenteken bestaat al een aanbod.',
+        ],
+    ],
+
+    'attributes' => [
+        'plate' => 'kenteken',
+        'brand' => 'merk',
+        'model' => 'model',
+        'price' => 'vraagprijs',
+        'mileage' => 'kilometerstand',
+        'seats' => 'zitplaatsen',
+        'doors' => 'aantal deuren',
+        'weight' => 'massa rijklaar',
+        'production_year' => 'jaar van productie',
+        'color' => 'kleur',
+    ],
+];
