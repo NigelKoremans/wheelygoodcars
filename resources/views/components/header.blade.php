8ace11ccdb543ceddc7ee9d74418b1ce5d3a3464
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-30 15:30:36 +0200

    added route to all offers link

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="flex items-center px-4 pt-2 pb-3 border-b border-black">
         <a class="text-xl font-bold" href="{{ route('home') }}"><span class="text-white text-shadow-black text-border">Wheely</span> Good Cars<span class="text-white text-shadow-black text-border">!</span></a>
-        <a class="ml-6 font-bold" href="">Alle auto's</a>
+        <a class="ml-6 font-bold" href="{{ route('offers.index') }}">Alle auto's</a>
         <a class="ml-6 font-bold" href="{{ route('offers.start') }}">Aanbod plaatsen</a>
         @auth
         <div class="ml-auto flex flex-row space-x-4">
