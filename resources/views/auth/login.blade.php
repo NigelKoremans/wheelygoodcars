f8340204288fd2287f27013f89227d71e900bf61
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-30 15:21:56 +0200

    added error messages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,15 +3,36 @@
         <form class="mx-auto my-auto flex flex-col h-full flex-1" action="{{ route('login.store') }}" method="POST">
             <div class="border border-neutral-500 py-8 px-8">
                 @csrf
+
+                @if ($errors->any())
+                <div class="mb-4 rounded border border-red-300 bg-red-100 p-2 text-red-800">
+                    <p class="font-semibold">Controleer je invoer:</p>
+                    <ul class="list-disc pl-5">
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+
                 <div class="py-2">
                     <label for="email">E-mail</label>
-                    <input placeholder="Enter your emailaddress" class="block p-1 w-64 bg-white shadow-sm border border-gray-300" type="text" name="email" id="email">
+                    <input placeholder="Voer je e-mailadres in" class="block p-1 w-64 bg-white shadow-sm border border-gray-300" type="email" name="email" id="email" value="{{ old('email') }}" required autocomplete="username" autofocus>
+                    @error('email')
+                    <p class="mt-1 text-sm text-red-200">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="py-2">
-                    <label for="password">Password</label>
-                    <input placeholder="Enter your password" class="block p-1 w-64 bg-white shadow-sm border border-gray-300" type="password" name="password" id="password">
+                    <label for="password">Wachtwoord</label>
+                    <input placeholder="Voer je wachtwoord in" class="block p-1 w-64 bg-white shadow-sm border border-gray-300" type="password" name="password" id="password" required autocomplete="current-password">
+                    @error('password')
+                    <p class="mt-1 text-sm text-red-200">{{ $message }}</p>
+                    @enderror
                 </div>
-                <input class="cursor-pointer text-black px-4 py-1 mt-4 text-lg" type="submit" value="Login">
+                <div class="mt-4 flex items-center justify-between">
+                    <a href="{{ route('register') }}">Geen account?</a>
+                    <input class="cursor-pointer text-black px-4 py-1 text-lg" type="submit" value="Inloggen">
+                </div>
             </div>
         </form>
     </div>
