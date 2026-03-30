b0c140ed41c42cfb077bf1ae61b3a82e6aad6cb1
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-30 15:23:03 +0200

    created CarListItem
    used for showing a car as a list
    
    used in offers.index and offers.myoffers

---
+++ resources/views/components/car-list-item.blade.php
@@ -0,0 +1,30 @@
+<div class="w-full mb-4 py-4 space-y-2">
+    <div class="border border-neutral-300 w-full col-span-3"></div>
+    <a href="{{ route('offers.show', $car->id)}}" class="text-xl font-bold">{{ ucfirst($car->make) }} {{ ucfirst($car->model) }}</a>
+
+    <div class="grid grid-cols-3 md:grid-cols-3 mt-4">
+        <a href="{{ route('offers.show', $car->id)}}">
+            @if($car->image)
+            <img src="{{ asset('images/' . $car->image) }}" alt="{{ $car->make }} {{ $car->model }}" class="w-full h-auto md:w-auto md:h-28 col-span-2 md:col-span-1 aspect-video object-cover rounded shadow-sm">
+            @else
+            <div class="w-full h-auto md:w-auto md:h-28 aspect-video bg-gray-200 flex items-center justify-center rounded shadow-sm">
+                <span class="text-gray-500">No image available</span>
+            </div>
+            @endif
+        </a>
+
+        <div class="">
+            <p class="text-xl font-semibold text-green-600">€ {{ number_format($car->price, 2, ',', '.') }}</p>
+            <p><strong>Kenteken:</strong> {{ $car->license_plate }}</p>
+            <p><strong>Kilometerstand:</strong> {{ number_format($car->mileage, 0, ',', '.') }} km</p>
+            <p><strong>Jaar van productie:</strong> {{ $car->production_year ?? '-' }}</p>
+        </div>
+
+        <div class="">
+            <p><strong>Kleur:</strong> {{ ucfirst($car->color) ?? '-' }}</p>
+            <p><strong>Zitplaatsen:</strong> {{ $car->seats ?? '-' }}</p>
+            <p><strong>Deuren:</strong> {{ $car->doors ?? '-' }}</p>
+            <p><strong>Massa:</strong> {{ $car->weight ? $car->weight . ' kg' : '-' }}</p>
+        </div>
+    </div>
+</div>
