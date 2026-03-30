fbdeaebc3958b19b0a3685dde3aedb14cbe7ec35
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-30 15:30:26 +0200

    moved my offers and made auth only

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,13 +2,16 @@
     <nav class="flex items-center px-4 pt-2 pb-3 border-b border-black">
         <a class="text-xl font-bold" href="{{ route('home') }}"><span class="text-white text-shadow-black text-border">Wheely</span> Good Cars<span class="text-white text-shadow-black text-border">!</span></a>
         <a class="ml-6 font-bold" href="">Alle auto's</a>
-        <a class="ml-6 font-bold" href="">Mijn aanbod</a>
         <a class="ml-6 font-bold" href="{{ route('offers.start') }}">Aanbod plaatsen</a>
         @auth
-        <form class="ml-auto font-bold" action="{{ route('logout') }}" method="post">
-            @csrf
-            <input class="cursor-pointer hover:underline" type="submit" value="uitloggen">
-        </form>
+        <div class="ml-auto flex flex-row space-x-4">
+            <a href="{{ route('offers.myoffers') }}" class="font-bold">Mijn aanbod</a>
+            <form class="font-bold" action="{{ route('logout') }}" method="post">
+                @csrf
+                <input class="cursor-pointer hover:underline" type="submit" value="Uitloggen">
+            </form>
+        </div>
+
         @else
         <a class="ml-auto font-bold" href="{{ route('login') }}">Inloggen</a>
         @endauth
