9228050d6783f02a86fb5f8bdf1d3c993a3a26dc
Nigel Koremans <nigel.koremans@gmail.com>
2026-03-30 15:18:41 +0200

    changed relation with tag to belongstomany

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Car extends Model
 {
@@ -30,8 +30,8 @@ class Car extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function tags() : HasMany
+    public function tags() : BelongsToMany
     {
-        return $this->hasMany(Tag::class);
+        return $this->belongsToMany(Tag::class);
     }
 }
