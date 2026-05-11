d551aaea6f5c3624d32ab4d5d0b26cf2d98c1769
Nigel Koremans <nigel.koremans@gmail.com>
2026-05-11 10:39:26 +0200

    added wire:navigate to links

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
 <header>
     <nav class="flex items-center px-4 pt-2 pb-3 border-b border-black">
-        <a class="text-xl font-bold" href="{{ route('home') }}"><span class="text-white text-shadow-black text-border">Wheely</span> Good Cars<span class="text-white text-shadow-black text-border">!</span></a>
-        <a class="ml-6 font-bold" href="{{ route('offers.index') }}">Alle auto's</a>
-        <a class="ml-6 font-bold" href="{{ route('offers.start') }}">Aanbod plaatsen</a>
+        <a class="text-xl font-bold" href="{{ route('home') }}" wire:navigate>Wheely <span class="text-border text-shadow-white text-black">Good Cars</span>!</a>
+        <a class="ml-6 font-bold" href="{{ route('offers.index') }}" wire:navigate.hover>Alle auto's</a>
+        <a class="ml-6 font-bold" href="{{ route('offers.start') }}" wire:navigate>Aanbod plaatsen</a>
         @auth
         <div class="ml-auto flex flex-row space-x-4">
-            <a href="{{ route('offers.myoffers') }}" class="font-bold">Mijn aanbod</a>
+            <a href="{{ route('offers.myoffers') }}" wire:navigate class="font-bold">Mijn aanbod</a>
             <form class="font-bold" action="{{ route('logout') }}" method="post">
                 @csrf
                 <input class="cursor-pointer" type="submit" value="Uitloggen">
