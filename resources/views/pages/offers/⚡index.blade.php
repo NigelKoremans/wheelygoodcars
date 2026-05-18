869b2396c742a38a4992bd44cc2805993f3c64df
Nigel Koremans <nigel.koremans@gmail.com>
2026-05-18 14:28:34 +0200

    Use Blade @forelse for offers list

--- resources/views/pages/offers/⚡index.blade.php
+++ resources/views/pages/offers/⚡index.blade.php
@@ -21,14 +21,13 @@ new #[Title('Alle Auto\'s - Wheely Good Cars!')] class extends Component
 <div class="w-full py-8 px-8">
     <h1 class="font-bold text-3xl py-4">Alle Auto's</h1>
     <div class="border w-full bg-white">
-        @foreach ($offers as $car)
+        @forelse ($offers as $car)
         <x-car-list-item :$car />
-        @endforeach
-        @if ($offers->isEmpty())
+        @empty
         <div class="p-6 text-center text-gray-600">
             <p class="text-lg font-medium">Er zijn nog geen auto's toegevoegd.</p>
         </div>
-        @endif
+        @endforelse
     </div>
     {{ $offers->links() }}
 </div>
